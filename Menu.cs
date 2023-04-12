0ea5696f3d7eec6c18187d88446b2134e1e06217
Victor Luiz Domingues <victorl.domingues@hotmail.com>
2023-04-12 01:51:53 -0300

    melhorias de complexidade ciclomatica

---
+++ Menu.cs
@@ -0,0 +1,41 @@
+public class Menu
+{
+    private readonly Arvore _arvore;
+    private readonly IEnumerable<Acao> _acoes;
+    public Menu()
+    {
+        _arvore = new();
+        _acoes = Enum.GetValues(typeof(Acao))
+                     .Cast<Acao>();
+    }
+
+    public void Imprimir()
+    {
+        foreach (var item in _acoes)
+            Console.WriteLine($"Ação: ({item.GetHashCode()}) - {item}");
+
+        Console.WriteLine("\nInforme o código da ação:");
+    }
+
+    public void Inserir(string linha)
+        => _arvore.Inserir(linha);
+
+    public void Buscar(string linha)
+        => Console.WriteLine($"Existe {linha}: {_arvore.Buscar(linha)}");
+
+    public void Remover(string linha)
+        => _arvore.Remover(linha);
+
+    public void Executar(Acao acao)
+    {
+        if (acao == Acao.Sair)
+            Environment.Exit(0);
+
+        Console.WriteLine($"\nAção {acao} - Informe a palavra:");
+        var linha = Console.ReadLine().ToLowerInvariant();
+        var method = typeof(Menu).GetMethod(acao.ToString());
+        method.Invoke(this, new object[] { linha });
+        Console.WriteLine();
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,49 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System;
 
-var arvore = new Arvore();
+var menu = new Menu();
 
 while (true)
 {
-
-    foreach (var item in Enum.GetValues(typeof(Acao))
-    .Cast<Acao>())
-    {
-        Console.WriteLine($"Ação: {item.GetHashCode()} - {item}");
-    }
-
+    menu.Imprimir();
     var linha = Console.ReadLine();
-
     Enum.TryParse<Acao>(linha, true, out var acao);
-    Console.WriteLine(acao);
-
-    switch (acao)
-    {
-        case Acao.Inserir:
-            {
-                Console.WriteLine("Inserir - Informe a palavra:");
-                linha = Console.ReadLine();
-                arvore.Inserir(linha);
-                break;
-            }
-
-        case Acao.Buscar:
-            {
-                Console.WriteLine("Buscar - Informe a palavra:");
-                linha = Console.ReadLine();
-                var existe = arvore.Buscar(linha);
-                Console.WriteLine($"Existe {linha}: {existe}");
-                break;
-            }
-        case Acao.Remover:
-            {
-                Console.WriteLine("Remover - Informe a palavra:");
-                linha = Console.ReadLine();
-                arvore.Remover(linha);
-                break;
-            }
-        case Acao.Sair:
-            return;
-    }
-
+    menu.Executar(acao);
 }
